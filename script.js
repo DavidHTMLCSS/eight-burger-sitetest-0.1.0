dde4497dec95bb8f5ecf846d235cdbfe6186e474
DavidHTMLCSS <138385696+DavidHTMLCSS@users.noreply.github.com>
2023-12-02 23:00:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,80 @@
+// Check screen for smartphones
+
+window.addEventListener('load', function() {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+        const style = document.createElement('style');
+        style.textContent = `
+            header {
+                flex-direction: column;
+                align-items: stretch;
+                padding: 15px 10px;
+            }
+
+            .order-button {
+                display: block;
+                position: absolute;
+                padding: 5px 15px;
+                bottom: 10px;
+                left: 50%;
+                transform: translateX(-50%);
+                z-index: 999;
+            }
+
+            .logo_nav {
+                width: 60px;
+                padding: 0;
+            }
+
+            nav {
+                display: none;
+                position: absolute;
+                top: 100%;
+                right: 0;
+                background-color: red;
+                border-radius: 10px;
+                padding: 15px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            }
+
+            nav ul {
+                justify-content: space-around;
+                flex-direction: column;
+                align-items: center;
+            }
+
+            nav ul li {
+                padding: 5px 0;
+            }
+
+            .menu-toggle {
+                display: block;
+            }
+
+            .menu-toggle.active + nav {
+                display: flex;
+                flex-direction: column;
+            }
+
+            .menu-toggle.active .bar:nth-child(1) {
+                transform: rotate(-45deg) translate(-5px, 6px);
+            }
+
+            .menu-toggle.active .bar:nth-child(2) {
+                opacity: 0;
+            }
+
+            .menu-toggle.active .bar:nth-child(3) {
+                transform: rotate(45deg) translate(-5px, -6px);
+            }
+
+            button {
+                padding: 8px 25px;
+            }
+        `;
+        document.head.append(style);
+    }
+});
+
 // navigation button for smartphones
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     this.classList.toggle('active');
