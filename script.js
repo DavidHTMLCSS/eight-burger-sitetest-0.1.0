f8c597bacf4250ec17970d9ce86900e92767bcb3
DavidHTMLCSS <138385696+DavidHTMLCSS@users.noreply.github.com>
2023-12-11 17:28:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-// Check screen for smartphones
+document.addEventListener('DOMContentLoaded', function() {
+    const aboutSection = document.getElementById('about');
+    const aboutButton = document.getElementById('about-button');
 
-window.addEventListener('load', function() {
-    if (window.matchMedia('(max-width: 768px)').matches) {
-        // Добавляем классы для мобильной версии
-        document.querySelector('header').classList.add('mobile-header');
-        document.querySelector('nav').classList.add('mobile-nav');
-        // ... Другие классы для мобильной версии, если нужно
-    }
-});
-
-// navigation button for smartphones
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-    var nav = document.querySelector('nav');
-    nav.classList.toggle('active');
-});
-
-document.querySelector('.order-button').addEventListener('click', function() {
-    var nav = document.querySelector('nav');
-    if (nav.classList.contains('active')) {
-        nav.classList.remove('active');
-        document.querySelector('.menu-toggle').classList.remove('active');
-    }
+    aboutButton.addEventListener('click', function() {
+        aboutSection.scrollIntoView({ behavior: 'smooth' });
+    });
 });
